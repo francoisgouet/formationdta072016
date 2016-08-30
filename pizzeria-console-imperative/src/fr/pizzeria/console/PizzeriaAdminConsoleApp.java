1e751d482e872fc31b1a47bd26bb253a5d22da80
GOUET Francois <gouetfrancois@gmail.com>
2016-08-30 14:45:24 +0200

    Well done TP3

--- pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -2,23 +2,20 @@ package fr.pizzeria.console;
 
 import java.util.Scanner;
 
-import fr.pizzeria.model.Pizza;
-
 public class PizzeriaAdminConsoleApp {
 	
 	static boolean menu = true;
 	
 	public static Scanner in = new Scanner(System.in);
 	
-	public static Pizza[] tab = {
-			new Pizza(0,"PEP","PÃ©pÃ©roni",12.50),
-			new Pizza(1,"MAR","Margherita",14.00),
-			new Pizza(2,"REI","La reine",11.50),
-			new Pizza(3,"FRO","La 4 Fromtons",12.00),
-			new Pizza(4,"CAN","La cannibale",12.50),
-			new Pizza(5,"SAV","La savoyarde",13.00),
-			new Pizza(6,"ORI","L'orientale",13.50),
-			new Pizza(7,"IND","L'indienne",14.00)};
+	public static String[][] tab = new String[][]{{"0","PEP","Pépéroni","12.50"},
+		{"1","MAR","Margherita","14.00"},
+		{"2","REI","La reine","11.50"},
+		{"3","FRO","La 4 Fromtons","12.00"},
+		{"4","CAN","La cannibale","12.50"},
+		{"5","SAV","La savoyarde","13.00"},
+		{"6","ORI","L'orientale","13.50"},
+		{"7","IND","L'indienne","14.00"}};
 		
 	public static void main(String[] args){
 		int choix;
@@ -36,7 +33,7 @@ public class PizzeriaAdminConsoleApp {
 				tab = ajouter_pizzas();
 				break;
 			case 3:
-				System.out.println("Mise ï¿½ jour d'une pizza");
+				System.out.println("Mise à jour d'une pizza");
 				tab = maj_pizzas();
 				break;
 			case 4:
@@ -53,37 +50,47 @@ public class PizzeriaAdminConsoleApp {
 		}
 	}
 	
-	private static Pizza[] supp_pizzas() {
+	private static String[][] supp_pizzas() {
 		lister_pizzas();
 		System.out.println("Veuillez choisir la pizza a supprimer (99 pour abondonner)");
-		int index = in.nextInt()-1;		Pizza [] tab_tmp = new Pizza[tab.length-1];
-		int ind;
+		int index = in.nextInt()-1;
+		String [][] tab_tmp = new String[tab.length-1][4];
+		// il parcourt juska l'index
 		if (index < tab.length){
-			for (int i = 0; i < tab_tmp.length; i++) {
-				if (i>=index){
-					ind = i+1;
-				}
-				else{
-					ind = i;
-				}
-				tab_tmp[i] = tab[ind];
+			for (int i = 0; i < index; i++) {
+				tab_tmp[i][0] = tab[i][0];
+				tab_tmp[i][1] = tab[i][1];
+				tab_tmp[i][2] = tab[i][2];
+				tab_tmp[i][3] = tab[i][3];
 			}
+		for (int j = index; j < tab_tmp.length; j++) {
+			tab_tmp[j][0] = tab[j+1][0];
+			tab_tmp[j][1] = tab[j+1][1];
+			tab_tmp[j][2] = tab[j+1][2];
+			tab_tmp[j][3] = tab[j+1][3];
+		}
 		}
 		return tab_tmp;
 	}
 	
-	private static Pizza[] ajouter_pizzas() {
+	private static String [][] ajouter_pizzas() {
 		System.out.println("Veuillez	saisir	le	code");
 		String code = in.next();
 		System.out.println("Veuillez	saisir	le	nom	(sans	espace)");
 		String nom = in.next();
 		System.out.println("Veuillez	saisir	le	prix");
-		Double prix = in.nextDouble();
-		Pizza[] tab2 = new Pizza[tab.length+1];
+		String prix = in.next();
+		String [][] tab2 = new String[tab.length+1][4];
 		for (int i = 0; i < tab.length; i++) {
-				tab2[i] = tab[i];
+				tab2[i][0] = tab[i][0];	
+				tab2[i][1] = tab[i][1];	
+				tab2[i][2] = tab[i][2];
+				tab2[i][3] = tab[i][3];
 		}
-		tab2[tab2.length-1] = new Pizza(tab2.length-1,code,nom,prix);
+		tab2[tab2.length-1][0] = Integer.toString(tab.length+1);
+		tab2[tab2.length-1][1] = code; 
+		tab2[tab2.length-1][2] = nom; 
+		tab2[tab2.length-1][3] = prix;
 		menu = true;
 		return tab2;
 	}
@@ -92,21 +99,19 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("*****	Pizzeria	Administration	*****");
 		System.out.println("1.	Lister	les	pizzas");
 		System.out.println("2.	Ajouter une	nouvelle	pizza");	
-		System.out.println("3.	Mettre	Ã 	jour	une	pizza");
+		System.out.println("3.	Mettre	à	jour	une	pizza");
 		System.out.println("4.	Supprimer	une	pizza");
 		System.out.println("99.	Sortir");
 		}
 	
 	public static void lister_pizzas(){
-		for (Pizza piz:tab) {
-			//System.out.println(tab[s].getCode());
-			System.out.println(piz.getCode()+ "->" + piz.getNom()+"("+ piz.getPrix()+"â‚¬)");
+		for (int s = 0;s < tab.length;s++) {
+			System.out.println(tab[s][1]+"->"+tab[s][2]+"("+tab[s][3]+"€)");
 			}
-		System.out.println(new String("--------------"+Pizza.NB_PIZZAS+" pizzas "+" depuis l'initialisation du programme"));
 		menu = true;
 	}
 	
-	public static Pizza[] maj_pizzas(){
+	public static String[][] maj_pizzas(){
 		lister_pizzas();
 		System.out.println("Veuillez choisir la pizza a modifier (99 pour abondonner)");
 		int choix = in.nextInt();
@@ -119,11 +124,11 @@ public class PizzeriaAdminConsoleApp {
 			System.out.println("Veuillez	saisir	le	nom	(sans	espace)");
 			String nom = in.next();
 			System.out.println("Veuillez	saisir	le	prix");
-			Double prix = in.nextDouble();
+			String prix = in.next();
 			// menu = false;
-			tab[choix-1].setCode(code);
-			tab[choix-1].setNom(nom);
-			tab[choix-1].setPrix(prix);
+			tab[choix][1] = code;
+			tab[choix][2]= nom;
+			tab[choix][3] = prix;
 			}
 	return tab;
 	}	
